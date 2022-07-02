fb173f162194916652e68a758b82244274307a3c
Nurul Haque Mazumder <nurul@batworld.com>
2022-07-02 14:28:57 +0600

    added all basic types of button for testing CRUD operations

--- mfe2/src/app/service/account.service.ts
+++ mfe2/src/app/service/account.service.ts
@@ -4,13 +4,30 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class AccountService {
+  baseUrl: string = 'http://localhost:9638/account';
+
   constructor(private http: HttpClient) { }
 
-  getAccountDetailsResponse() {
+  getUserInfo(): Observable<HttpResponse<any>> {
     // The Observable returned by get() is of type Observable<string>
     // because a text response was specified.
     // There's no need to pass a <string> type parameter to get().
-    return this.http.get(
-      'http://localhost:9638/account/view/1', { observe: 'response', responseType: 'text' });
+    return this.http.get<any>(
+      `${this.baseUrl}/view/1`, { observe: 'response' });
+  }
+
+  createUserInfo(): Observable<HttpResponse<any>> {
+    return this.http.post<any>(
+      `${this.baseUrl}/create/1`, { observe: 'response' });
+  }
+
+  updateUserInfo(): Observable<HttpResponse<any>> {
+    return this.http.put<any>(
+      `${this.baseUrl}/update/1`, { observe: 'response' });
+  }
+
+  deleteUserInfo(): Observable<HttpResponse<any>> {
+    return this.http.delete<any>(
+      `${this.baseUrl}/delete/1`, { observe: 'response' });
   }
 }
--- mfe2/src/app/userdashboard/userprofile/userprofile.component.ts
+++ mfe2/src/app/userdashboard/userprofile/userprofile.component.ts
@@ -20,7 +20,7 @@ export class UserprofileComponent implements OnInit {
   public userProfile: KeycloakProfile | null = null;
   keycloakInfoService$ = this.store.select(selectToken);
   allowedScopes: string[] = [];
-  responseReceived: string | null = null;
+  responseReceived: any | null = null;
 
   constructor(private readonly keycloak: KeycloakService, private readonly store: Store, private readonly accountService: AccountService) {
 
@@ -52,7 +52,7 @@ export class UserprofileComponent implements OnInit {
 
   setScopes() {
     const permissions: Permission[] = JSON.parse(localStorage.getItem('permissions')!);
-    permissions.forEach(permission => {
+    permissions?.forEach(permission => {
       permission.scopes.forEach(scope => this.allowedScopes.push(scope))
     })
   }
@@ -70,19 +70,31 @@ export class UserprofileComponent implements OnInit {
   }
 
   public logout() {
+    localStorage.clear()
     this.keycloak.logout();
   }
 
-  async showAccountDetails() {
-    this.accountService.getAccountDetailsResponse().subscribe(resp => {
+  showAccountDetails() {
+    this.accountService.getUserInfo().subscribe(resp => {
       // display its headers
       const keys = resp.headers.keys();
       const headers = keys.map(key =>
         `${key}: ${resp.headers.get(key)}`);
-      console.log("🚀 ~ file: userprofile.component.ts ~ line 72 ~ UserprofileComponent ~ this.accountService.getAccountDetailsResponse ~ headers", headers)
 
       this.responseReceived = resp.body;
     }, error => console.log(error)
     )
+  }
+
+  createUser() {
+    this.accountService.createUserInfo().subscribe(resp => this.responseReceived = resp, error => console.log(error))
+  }
+
+  updateUser() {
+    this.accountService.updateUserInfo().subscribe(resp => this.responseReceived = resp, error => console.log(error))
+  }
+
+  deleteUser() {
+    this.accountService.deleteUserInfo().subscribe(resp => this.responseReceived = resp.body, error => console.log(error))
   }
 }
